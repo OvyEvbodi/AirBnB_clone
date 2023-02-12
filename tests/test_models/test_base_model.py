40ecedea88e31ebf79ae91a4250d1538c64c1ffc
Ovy-Evbodi <evbodiovo@gmail.com>
2023-02-13 00:40:43 +0100

    Fix(*): Add new tests

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,72 +1,93 @@
 #!/usr/bin/python3
-"""Test module tests the ``BaseModel`` class, using unittesting"""
+"""This is the unittest file for `BaseModel` class"""
 
 import unittest
-from models.base_model import BaseModel
+import uuid
 from datetime import datetime
+from models.base_model import BaseModel
+from models import storage
 
 
-class TestBase(unittest.TestCase):
-    """Test class for unittests.
-    It inherits from unittest's ``TestCase``
+class TestBaseModel(unittest.TestCase):
+    """Defines a set of test cases for the `BaseModel` class"""
 
-    A ``BaseModel`` instance will hereafter
-    be simply referred to as an "instance"
-    """
+    def setUp(self):
+        """Sets up test cases"""
+        self.model = BaseModel()
 
-    def test_instance(self):
-        """Checks that an instance is created properly"""
+    def test_init_args(self):
+        """Test the constructor of BaseModel with arguments"""
 
-        base1 = BaseModel()
-        base2 = BaseModel()
-        base3 = BaseModel()
-        self.assertTrue(isinstance(base1, BaseModel))
-        self.assertTrue(isinstance(base2, BaseModel))
-        self.assertTrue(isinstance(base3, BaseModel))
-        self.assertFalse(isinstance(base1, int))
-        self.assertFalse(isinstance(base2, str))
-        self.assertFalse(isinstance(base3, list))
+        model = BaseModel(1, 2)
+        self.assertEqual(str, type(model.id))
+        self.assertEqual(datetime, type(model.created_at))
+        self.assertEqual(datetime, type(model.updated_at))
 
-    def test_attributes(self):
-        """Checks that an instance
-        correctly initializes its instance attributes
-        """
-        base1 = BaseModel()
-        base2 = BaseModel()
-        self.assertTrue(isinstance(base1.created_at, datetime))
-        self.assertTrue(isinstance(base2.created_at, datetime))
-        self.assertTrue(isinstance(base1.updated_at, datetime))
-        self.assertTrue(isinstance(base2.updated_at, datetime))
-        self.assertTrue(base1.id)
-        self.assertTrue(base2.id)
-        self.assertEqual(len(base1.id), 36)
-        self.assertEqual(len(base2.id), 36)
-        self.assertNotEqual(len(base1.id), 20)
-        self.assertNotEqual(len(base2.id), 20)
-        self.assertTrue(isinstance(base1.id, str))
-        self.assertTrue(isinstance(base2.id, str))
-        self.assertFalse(isinstance(base1.id, tuple))
-        self.assertFalse(isinstance(base1.id, list))
+    def test_init_with_kwargs(self):
+        """Test the constructor with key word arguments"""
+
+        model_id = str(uuid.uuid4())
+        created_at = datetime.now()
+        updated_at = datetime.now()
+        kwargs = {
+            "id": model_id,
+            "created_at": created_at.isoformat(),
+            "updated_at": updated_at.isoformat()
+        }
+        model = BaseModel(**kwargs)
+        self.assertEqual(model.id, model_id)
+        self.assertEqual(model.created_at, created_at)
+        self.assertEqual(model.updated_at, updated_at)
+
+        kwargs = {"id": "a76d7d92-12c8-4908-b60d-6d9b9c8e6e97",
+                  "created_at": "2022-09-12T10:00:00.000000",
+                  "updated_at": "2022-09-12T10:30:00.000000"}
+
+        new_instance = BaseModel(**kwargs)
+        self.assertIsInstance(new_instance, BaseModel)
+        self.assertEqual(new_instance.id, kwargs["id"])
+        self.assertIsInstance(new_instance.created_at, datetime)
+        self.assertIsInstance(new_instance.updated_at, datetime)
+        self.assertEqual(new_instance.created_at.isoformat(),
+                         kwargs["created_at"].split(".")[0])
+        self.assertEqual(new_instance.updated_at.isoformat(),
+                         kwargs["updated_at"].split(".")[0])
+
+    def test_init_without_kwargs(self):
+        """Test the constructor without key word arguments or positional ars"""
+
+        model = BaseModel()
+        self.assertIsInstance(model.id, str)
+        self.assertIsInstance(model.created_at, datetime)
+        self.assertIsInstance(model.updated_at, datetime)
+
+    def test_str(self):
+        """Test the magic `__str__` method"""
+
+        model_str = str(self.model)
+        self.assertIn("BaseModel", model_str)
+        self.assertIn(self.model.id, model_str)
+        self.assertIn(str(self.model.__dict__), model_str)
+
+    def test_save(self):
+        """Test the `save` method"""
+
+        updated_at = self.model.updated_at
+        self.model.save()
+        self.assertNotEqual(updated_at, self.model.updated_at)
+        self.assertIsInstance(storage.all(), dict)
+        self.assertTrue(type(self.model).__name__ + "." +
+                        self.model.id in storage.all().keys())
 
     def test_to_dict(self):
-        """Checks that instances are converted
-        to dictionary representations
-        """
-        base1 = BaseModel()
-        base_dict = base1.to_dict()
-        base2 = BaseModel(base_dict)
-        self.assertTrue(type(base_dict) == dict)
-        self.assertTrue(type(base2.to_dict()) == dict)
-        self.assertFalse(type(base_dict) != dict)
+        """Test the `to_dict` method"""
 
-    def test_docs(self):
-        """Checks that an instance is documented"""
-        base1 = BaseModel()
-        base_dict = base1.to_dict()
-        base2 = BaseModel(base_dict)
-        self.assertTrue(base1.__str__())
-        self.assertTrue(base2.__doc__)
+        model_dict = self.model.to_dict()
+        self.assertIsInstance(model_dict, dict)
+        self.assertEqual(model_dict["__class__"], "BaseModel")
+        self.assertIsInstance(model_dict["created_at"], str)
+        self.assertIsInstance(model_dict["updated_at"], str)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
